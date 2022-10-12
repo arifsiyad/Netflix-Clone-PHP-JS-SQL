bc0e7dbe018e65b8aa5c60814c64bfd55408d78c
Arif Siyad <arifsiyad@gmail.com>
2022-10-12 03:03:35 -0700

    added register page.

---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+   <head>
+     <title>Welcome to Reeceflix </title>
+
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+
+   </head>
+
+<body> 
+
+hotdogs
+
+</body>
+
+</html>
