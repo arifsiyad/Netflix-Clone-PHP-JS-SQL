aa316318608b46022ea9dc3b5515eb3eb64c5c2b
Arif Siyad <arifsiyad@gmail.com>
2022-10-12 23:10:06 -0700

    Added css and style.

--- register.php
+++ register.php
@@ -1,3 +1,9 @@
+<?php
+
+    if(isset($_POST["submitButton"]))
+    echo "Form submitted"
+?>
+
 <!DOCTYPE html>
 <html>
    <head>
@@ -6,10 +12,50 @@
     <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
 
    </head>
-
 <body> 
 
-hotdogs
+
+<div class="signInContainer">
+
+    <div class="column">
+
+    <div class="header">
+        <h3> Sign Up </h3>
+        <span> to continue to Reddflix </span>
+
+        <form method="POST">
+
+
+            <input type="text" name="firstName" placeholder="First Name" required>
+
+            <input type="text" name="lastName" placeholder="Last Name" required>
+
+            <input type="text" name="username" placeholder="Username" required>
+
+            <input type="email" name="Email" placeholder="Email" required>
+
+            <input type="email" name="Email2" placeholder="Confirm Email" required>
+
+            <input type="password" name="password" placeholder="Password" required>
+
+            <input type="password" name="password2" placeholder="Confirm Password" required>
+
+            <input type="submit" name="submitButton" value="SUBMIT">
+
+
+
+
+        </form>
+
+
+
+
+
+    </div>
+
+
+
+</div>
 
 </body>
 
